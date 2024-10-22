0e2ab20b08517d610b11a63bf58023cb47178159
Mariana-3b <130559220+Mariana-3b@users.noreply.github.com>
2024-10-22 12:13:38 -0300

    ...

--- graficos/InformacoesGlobais.js
+++ graficos/InformacoesGlobais.js
@@ -5,7 +5,8 @@ async function vizualizarInformaçoesGlobais() {
     const res = await fetch(url)
     const dados = await res.jason()
     console. log (dados)
-    
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
     
 }
 
