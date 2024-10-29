4d1aa291f87993d36a8d9375f94653e3408c0817
Mariana-3b <130559220+Mariana-3b@users.noreply.github.com>
2024-10-29 12:21:34 -0300

    ..

--- graficos/InformacoesGlobais.js
+++ graficos/InformacoesGlobais.js
@@ -4,6 +4,7 @@ console.log(url);
 async function vizualizarInformaçoesGlobais() {
     const res = await fetch(url)
     const dados = await res.jason()
+    console.log(dados)
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container_texto')
     paragrafo.innerHTML = `Sabia que no mundo tem ${dados.total_pessoas_mundo} de pessoas e aproximadamente ${dados.total_pessoas_conectadas} de pessoas passam em média ${dados.tempo_medio} horas conectadas em alguma rede social?`
