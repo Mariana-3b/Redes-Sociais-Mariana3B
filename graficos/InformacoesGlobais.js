4a01c06042732e161e36a58548985558151cd0a3
Mariana-3b <130559220+Mariana-3b@users.noreply.github.com>
2024-11-26 12:15:13 -0300

    mm

--- graficos/InformacoesGlobais.js
+++ graficos/InformacoesGlobais.js
@@ -7,7 +7,7 @@ async function vizualizarInformaçoesGlobais() {
     console.log(dados)
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container_texto')
-    paragrafo.innerHTML = `Sabia que no mundo tem ${dados.total_pessoas_mundo} de pessoas e aproximadamente ${dados.total_pessoas_conectadas} de pessoas passam em média ${dados.tempo_medio} horas conectadas em alguma rede social?`
+    paragrafo.innerHTML = `Sabia que no mundo tem <span>${dados.total_pessoas_mundo} de pessoas e aproximadamente ${dados.total_pessoas_conectadas} de pessoas passam em média ${dados.tempo_medio} horas conectadas em alguma rede social?`
     console.log(paragrafo)
 
     const container = document.getElementById('graficos-container')
