c3c515fd3076982fec7079296e481b43ca39e1cd
Mariana-3b <130559220+Mariana-3b@users.noreply.github.com>
2024-11-27 16:45:23 -0300

    mm

--- graficos/InformacoesGlobais.js
+++ graficos/InformacoesGlobais.js
@@ -1,17 +1,23 @@
 const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
-console.log(url);
-async function vizualizarInformaçoesGlobais() {
+async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
-    const dados = await res.jason()
-    console.log(dados)
+    const dados = await res.json()
+    
+    
+
+
     const paragrafo = document.createElement('p')
-    paragrafo.classList.add('graficos-container_texto')
-    paragrafo.innerHTML = `Sabia que no mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e aproximadamente <span>${dados.total_pessoas_conectadas}</span> de pessoas passam em média <span>${dados.tempo_medio}</span> horas conectadas em alguma rede social?`
-    console.log(paragrafo)
+    paragrafo.classList.add('graficos-container__texto')
+
+    
+    paragrafo.innerHTML = `Sabia que no mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e aproximadamente <span>${dados.total_pessoas_conectadas}</span> de pessoas passam em média <span>${dados.tempo_medio}</span> horas conectadas em alguma rede social?.`
+    
+   
 
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
 }
 
-vizualizarInformaçoesGlobais()
+
+vizualizarInformacoesGlobais()
